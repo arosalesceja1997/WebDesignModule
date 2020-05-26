02d64720a89d6e3a7490b412b84ec37a4c148c31
Anthony Tonatihu Rosales Ceja <arosalesceja1997@gmail.com>
2020-05-26 01:33:03 -0500

    correciones al escritorio

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,3 +22,38 @@ $(document).ready(function () {
   //   },
   // });
 });
+
+let thisurl = {
+  url: "",
+};
+
+let resp = {};
+let newUrl = "https://rel.ink/";
+
+function mostrar(acortar) {
+  thisurl.url = acortar;
+  var url = "https://rel.ink/api/links/";
+
+  fetch(url, {
+    method: "POST", // or 'PUT'
+    body: JSON.stringify(thisurl), // data can be `string` or {object}!
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .catch((error) => {
+      console.error("Error:", typeof error);
+      console.error("Error:", error);
+    })
+    .then((response) => {
+      if (typeof response.hashid === "undefined") {
+        alert("Ingresa una url valida");
+      } else {
+        alert("Tu url arcortada: \n" + newUrl + response.hashid);
+      }
+      document.getElementById("x-shorten").value = "" + ewUrl + response.hashid;
+      // console.info("Success:", typeof response.hashid);
+      // console.info("Success:", response);
+    });
+}
